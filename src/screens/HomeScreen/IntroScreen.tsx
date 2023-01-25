d2460151fad21562727db152d5d4214ad2025b74
Lucas Henrique <lukas.paes18@gmail.com>
2023-01-24 23:58:46 -0300

    feature: ajuste na pagina home screen

--- src/screens/HomeScreen/IntroScreen.tsx
+++ src/screens/HomeScreen/IntroScreen.tsx
@@ -12,35 +12,33 @@ export const HomeScreen = () => {
   const {navigate} = useNavigationHook();
 
   return (
-    <GestureMultiTouchContainer>
-      <Container padding={24} align="center" justify="center">
-        <Image source={LogoNomeRosa} />
-        <Button title="Junte-se" onPress={() => navigate('SignUpScreen')} />
-        <Text
-          style={{
-            fontSize: 14,
-            textAlign: 'center',
-            marginTop: 20,
-          }}>
-          Já faz parte ?
-        </Text>
+    <Container padding={24} align="center" justify="center">
+      <Image source={LogoNomeRosa} />
+      <Button title="Junte-se" onPress={() => navigate('SignUpScreen')} />
+      <Text
+        style={{
+          fontSize: 14,
+          textAlign: 'center',
+          marginTop: 20,
+        }}>
+        Já faz parte ?
+      </Text>
 
-        <View style={{display: 'flex', flexDirection: 'row', marginTop: 2}}>
-          <Text style={{fontSize: 14, textAlign: 'center'}}>Então clique</Text>
-          <TouchableOpacity onPress={() => navigate('SignInScreen')}>
-            <Text
-              style={{
-                fontSize: 14,
-                textAlign: 'center',
-                color: `#AF3476`,
-                marginLeft: 4,
-                fontWeight: 'bold',
-              }}>
-              aqui.
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Container>
-    </GestureMultiTouchContainer>
+      <View style={{display: 'flex', flexDirection: 'row', marginTop: 2}}>
+        <Text style={{fontSize: 14, textAlign: 'center'}}>Então clique</Text>
+        <TouchableOpacity onPress={() => navigate('SignInScreen')}>
+          <Text
+            style={{
+              fontSize: 14,
+              textAlign: 'center',
+              color: `#AF3476`,
+              marginLeft: 4,
+              fontWeight: 'bold',
+            }}>
+            aqui.
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </Container>
   );
 };
