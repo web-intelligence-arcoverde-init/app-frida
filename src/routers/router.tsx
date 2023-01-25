7c51bcd5b2c2ce2eb3d93dd5e6f8b824343f4a44
Lucas Henrique <lukas.paes18@gmail.com>
2023-01-24 23:53:30 -0300

    refactor: ajuste home screen

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {SplashScreen, IntroScreen} from '../screens';
+import {SplashScreen, IntroScreen, HomeScreen} from '../screens';
 
 const Stack = createNativeStackNavigator();
 
 export const AppRouters = () => {
   return (
     <Stack.Navigator
-      initialRouteName={'SplashScreen'}
+      initialRouteName={'HomeScreen'}
       screenOptions={{headerShown: false}}>
       <Stack.Screen name="SplashScreen" component={SplashScreen} />
       <Stack.Screen name="IntroScreen" component={IntroScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
--- src/screens/IntroScreen/IntroScreen.tsx
+++ src/screens/IntroScreen/IntroScreen.tsx
@@ -7,10 +7,13 @@ import {Dimensions, Image} from 'react-native';
 const {width} = Dimensions.get('screen');
 
 import {IMAGES} from '../../assets';
+import {useNavigationHook} from '../../hooks';
 
 const {IntroScreen01, IntroScreen02, IntroScreen03} = IMAGES;
 
 export const IntroScreen = () => {
+  const {navigate} = useNavigationHook();
+
   return (
     <Swiper>
       <Container>
@@ -36,7 +39,9 @@ export const IntroScreen = () => {
       </Container>
       <Container>
         <SwiperContainer
-          footer={<Button title="Entrar" />}
+          footer={
+            <Button title="Entrar" onPress={() => navigate('HomeScreen')} />
+          }
           title="What is Lorem Ipsum?"
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s">
           <Image source={IntroScreen01} style={{width: width, height: 400}} />
