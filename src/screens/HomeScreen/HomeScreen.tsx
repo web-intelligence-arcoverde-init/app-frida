8de870de7a6d1ba2fd6276ab65dea7968800411f
Lucas Henrique <lukas.paes18@gmail.com>
2023-02-15 00:47:03 -0300

    feature: localizacao

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,10 @@
-import {Image, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  Image,
+  PermissionsAndroid,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {Text} from '@rneui/themed';
 
 import {IMAGES} from '../../assets';
@@ -9,8 +15,90 @@ import {scale} from '../../utils';
 
 const {LogoNomeRosa} = IMAGES;
 
+import Geolocation from '@react-native-community/geolocation';
+import {useEffect, useState} from 'react';
+
+const authorizationLevelOptions: ('whenInUse' | 'always' | 'auto')[] = [
+  'whenInUse',
+  'always',
+  'auto',
+];
+
+const locationProviderOptions: ('playServices' | 'android' | 'auto')[] = [
+  'playServices',
+  'android',
+  'auto',
+];
+
 export const HomeScreen = () => {
   const {navigate} = useNavigationHook();
+
+  const [skipPermissionRequests, setSkipPermissionRequests] = useState(false);
+  const [authorizationLevel, setAuthorizationLevel] = useState<
+    'whenInUse' | 'always' | 'auto'
+  >('auto');
+
+  const [locationProvider, setLocationProvider] = useState<
+    'playServices' | 'android' | 'auto'
+  >('auto');
+
+  const requestAuthorization = async () => {
+    const authorization = await Geolocation.requestAuthorization();
+
+    console.log(authorization);
+  };
+
+  const watchPosition = () => {
+    try {
+      const watchID = Geolocation.watchPosition(
+        position => {
+          setPosition(JSON.stringify(position));
+        },
+        error => Alert.alert('WatchPosition Error', JSON.stringify(error)),
+      );
+      setSubscriptionId(watchID);
+    } catch (error) {
+      Alert.alert('WatchPosition Error', JSON.stringify(error));
+    }
+  };
+
+  const clearWatch = () => {
+    subscriptionId !== null && Geolocation.clearWatch(subscriptionId);
+    setSubscriptionId(null);
+    setPosition(null);
+  };
+
+  useEffect(() => {
+    Geolocation.setRNConfiguration({
+      skipPermissionRequests,
+      authorizationLevel,
+      locationProvider,
+    });
+  }, [skipPermissionRequests, authorizationLevel, locationProvider]);
+
+  const [position, setPosition] = useState<string | null>(null);
+  const [subscriptionId, setSubscriptionId] = useState<number | null>(null);
+  useEffect(() => {
+    return () => {
+      clearWatch();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getCurrentPosition = () => {
+    Geolocation.getCurrentPosition(
+      pos => {
+        console.log(JSON.stringify(pos));
+      },
+      error => Alert.alert('GetCurrentPosition Error', JSON.stringify(error)),
+      {enableHighAccuracy: true},
+    );
+  };
+
+  useEffect(() => {
+    watchPosition();
+    getCurrentPosition();
+  }, []);
 
   return (
     <Container padding={24} align="center" justify="center">
@@ -18,7 +106,11 @@ export const HomeScreen = () => {
         source={LogoNomeRosa}
         style={{width: scale(280), height: scale(360)}}
       />
-      <Button title="Entrar" onPress={() => navigate('SignInScreen')} />
+      {subscriptionId !== null ? (
+        <Button title="Clear Watch" onPress={clearWatch} />
+      ) : (
+        <Button title="Watch Position" onPress={watchPosition} />
+      )}
       <Text
         style={{
           fontSize: scale(12),
